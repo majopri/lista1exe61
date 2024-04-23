b5b2fb06c3ee78b8ec2502b4e18ecc8ee24e6a02
User <User@DESKTOP-G0A3IIV>
2024-04-23 00:17:01 -0300

    Adicionar arquivos de projeto.

---
+++ lista1exe61/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1exe61
+{
+    internal class Program
+    {
+        private static string mediaGeometrica;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite o primeiro valor:");
+            double valor1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("digite o segundo valor:");
+            double valor2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("A média geométrica de " + valor1 + " e " + valor2 + " é: " + mediaGeometrica);
+
+            double media = Math.Sqrt(valor1 * valor2);
+        }
+    }
+}
